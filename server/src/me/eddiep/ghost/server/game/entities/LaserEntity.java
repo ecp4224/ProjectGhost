fb18368c87a6a156a4d8e973852a73c5298e5f0d
Eddie Penta <ecp4224@gmail.com>
2015-06-12 14:21:08 -0400

    Laser should now do damage

--- server/src/me/eddiep/ghost/server/game/entities/LaserEntity.java
+++ server/src/me/eddiep/ghost/server/game/entities/LaserEntity.java
@@ -2,12 +2,17 @@ package me.eddiep.ghost.server.game.entities;
 
 import me.eddiep.ghost.server.game.Entity;
 import me.eddiep.ghost.server.game.entities.playable.Playable;
+import me.eddiep.ghost.server.game.util.Vector2f;
+import me.eddiep.ghost.server.utils.MathUtils;
 import me.eddiep.ghost.server.utils.TimeUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class LaserEntity extends Entity implements TypeableEntity {
     private Playable parent;
+    private ArrayList<Playable> alreadyHit = new ArrayList<>();
     public LaserEntity(Playable parent) {
         super();
         setParent(parent.getEntity());
@@ -24,10 +29,40 @@ public class LaserEntity extends Entity implements TypeableEntity {
         if (check) {
             float currentWidth = TimeUtils.ease(0f, 1040f, 300f, System.currentTimeMillis() - start);
 
-            float bx = (float) (parent.getEntity().getX() + Math.cos(rotation) * currentWidth);
-            float by = (float) (parent.getEntity().getY() + Math.sin(rotation) * currentWidth);
+            float x = getX(), y = getY() + 32f;
+            float bx = parent.getEntity().getX() + currentWidth;
+            float by = parent.getEntity().getY() + currentWidth - 32f;
 
-            
+                                                               //Center of rectangle
+            Vector2f[] rect = MathUtils.rotatePoints(rotation, new Vector2f(x + (currentWidth / 2f), y),
+                    new Vector2f(x, y),
+                    new Vector2f(bx, y),
+                    new Vector2f(bx, by),
+                    new Vector2f(x, by)
+            );
+
+            Playable[] opponents = parent.getOpponents();
+            for (Playable p : opponents) {
+                if (alreadyHit.contains(p))
+                    continue;
+
+                Entity toHit = p.getEntity();
+                if (MathUtils.isPointInside(toHit.getPosition(), rect)) {
+                    p.subtractLife();
+                    if (!toHit.isVisible()) {
+                        toHit.setVisible(true);
+                    }
+
+                    p.onDamage(parent); //p was damaged by the parent
+
+                    parent.onDamagePlayable(p); //the parent damaged p
+                    if (p.isDead()) {
+                        parent.onKilledPlayable(p);
+                    }
+
+                    alreadyHit.add(p);
+                }
+            }
         }
     }
 
