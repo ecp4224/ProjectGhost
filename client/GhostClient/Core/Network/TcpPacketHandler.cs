8159107f1fffe2686cd0a43c42b7fb0b37bbf74e
Eddie Penta <pentae@wit.edu>
2015-09-22 11:42:57 -0400

    Started working on client network protocol

---
+++ client/GhostClient/Core/Network/TcpPacketHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Ghost.Core.Network
+{
+    public class TcpPacketHandler : PacketHandler
+    {
+        private static readonly Dictionary<byte, Type> packets = new Dictionary<byte, Type>();
+
+        private TcpClient client;
+        public TcpPacketHandler(TcpClient client)
+        {
+            this.client = client;
+        }
+ 
+        public override Dictionary<byte, Type> Packets
+        {
+            get { return packets; }
+        }
+
+        public override void Start()
+        {
+
+        }
+    }
+}
