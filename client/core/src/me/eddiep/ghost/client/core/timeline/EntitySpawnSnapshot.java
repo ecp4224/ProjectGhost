85e2a453212f2f12b007c85aac56f031eb65c0e5
JohnPMerrill <john.merrill95@gmail.com>
2015-10-03 12:16:48 -0400

    started Kotlin implementation of ReplayHandler

---
+++ client/core/src/me/eddiep/ghost/client/core/timeline/EntitySpawnSnapshot.java
@@ -0,0 +1,50 @@
+package me.eddiep.ghost.client.core.timeline;
+
+public class EntitySpawnSnapshot {
+    short id;
+    String name;
+    float x, y;
+    double rotation;
+    boolean isPlayableEntity;
+    boolean isTypeableEntity;
+    boolean isParticle;
+    short type;
+
+    EntitySpawnSnapshot() { }
+
+    public short getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public boolean isPlayableEntity() {
+        return isPlayableEntity;
+    }
+
+    public boolean isTypeableEntity() {
+        return isTypeableEntity;
+    }
+
+    public short getType() {
+        return type;
+    }
+
+    public boolean isParticle() {
+        return isParticle;
+    }
+
+    public double getRotation() {
+        return rotation;
+    }
+}
