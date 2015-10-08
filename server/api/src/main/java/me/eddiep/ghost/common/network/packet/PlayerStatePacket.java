830e8111059239b28115fe890047ac45f75ea789
Eddie Penta <ecp4224@gmail.com>
2015-10-08 19:15:44 -0400

    Added invinciblity state in PlayerStatePackety

--- server/api/src/main/java/me/eddiep/ghost/common/network/packet/PlayerStatePacket.java
+++ server/api/src/main/java/me/eddiep/ghost/common/network/packet/PlayerStatePacket.java
@@ -24,6 +24,7 @@ public class PlayerStatePacket extends Packet<BaseServer, BasePlayerClient> {
             .write(player.getLives())
             .write(player.isDead())
             .write(player.isFrozen())
+            .write(player.)
             .endTCP();
     }
 }
